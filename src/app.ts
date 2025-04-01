d357f289f69fa4b1dc5b8d657820b59bd2ebfdce
mahmudtrainer <mahmudhasan.mm07@gmail.com>
2025-04-01 13:13:09 +0600

    create server.ts & app.ts

---
+++ src/app.ts
@@ -0,0 +1,69 @@
+import express, { Application, NextFunction, Request, Response } from "express";
+import bodyParser from "body-parser";
+import httpStatus from "http-status";
+import cors from "cors";
+import router from "./app/routes";
+import GlobalErrorHandler from "./app/middlewares/globalErrorHandler";
+import { PrismaClient } from "@prisma/client";
+import path from "path";
+
+const app: Application = express();
+const prisma = new PrismaClient();
+
+// Middleware setup
+prisma
+  .$connect()
+  .then(() => {
+    console.log("Database connected successfully!");
+  })
+  .catch((error) => {
+    console.error("Failed to connect to the database:", error);
+  });
+
+export const corsOptions = {
+  origin: [
+    // "https://tasneem-social-frontend.netlify.app",
+    "http://localhost:3000",
+    "http://192.168.11.130:3000",
+    "https://rayaa-social.vercel.app",
+    "https://tasneem-social-frontend.vercel.app",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+// Middleware setup
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+
+// Route handler for root endpoint
+app.get("/", (req: Request, res: Response) => {
+  res.send({
+    Message: "Welcome to api main route",
+  });
+});
+
+// Router setup
+app.use("/api/v1", router); // Assuming router handles '/api/v1' routes
+
+// Global Error Handler
+app.use(GlobalErrorHandler);
+
+// API Not found handler
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(httpStatus.NOT_FOUND).json({
+    success: false,
+    message: "API NOT FOUND!",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found!",
+    },
+  });
+});
+
+export default app;
